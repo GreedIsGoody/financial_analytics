944da031a1b2322a8eaa3721366e3afbeb60479b
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-08-02 19:02:36 +0300

    feat: outbox connection to main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
+import asyncio
 from fastapi import FastAPI 
 from app.core.config import settings 
 from app.api.v1.transactions import router as transactions_router
 from contextlib import asynccontextmanager
 from app.core.clickhouse import init_clickhouse
+from app.infrastructure.messaging.outbox_relayer import run_outbox_relayer
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     await init_clickhouse()
+    
+    relayer_task = asyncio.create_task(run_outbox_relayer(poll_interval=3))
+    
     yield
+    
+    relayer_task.cancel()
+    
 
 
 
