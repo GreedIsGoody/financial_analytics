260fac9fdd7e3f6b1c489ba75124c665913fcdb6
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-08-05 00:38:13 +0300

    feat: connecing a analytic router

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.api.v1.transactions import router as transactions_router
 from contextlib import asynccontextmanager
 from app.core.clickhouse import init_clickhouse
 from app.infrastructure.messaging.outbox_relayer import run_outbox_relayer
+from app.api.v1.analytics import router as analytics_router
 
 logging.basicConfig(
     level=logging.INFO,
@@ -35,6 +36,7 @@ async def lifespan(app:FastAPI):
 app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0", lifespan=lifespan)
 
 app.include_router(transactions_router, prefix="/api/v1")
+app.include_router(analytics_router, prefix="/api/v1")
 
 @app.get("/health")
 async def health_check():
