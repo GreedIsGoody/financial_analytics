13949fe2fcbe25025c298bb861cf4dbf6e25fb00
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-08-05 14:07:52 +0300

    refactor: restyling a pydentic schames

--- app/domain/analytics.py
+++ app/domain/analytics.py
@@ -1,19 +1,22 @@
-from decimal import Decimal 
-from pydantic import BaseModel 
-from uuid import UUID 
+from decimal import Decimal
+from uuid import UUID
+from pydantic import BaseModel, Field
+
 
 class StatusSummary(BaseModel):
     status: str
-    count: int 
-    total_amount: Decimal 
-    
+    count: int
+    total_amount: Decimal = Field(..., decimal_places=2, max_digits=18)
+
+
 class AnalyticsSummaryResponse(BaseModel):
-    total_transactions: int 
-    total_volume: Decimal 
-    avg_transaction_amount : Decimal
+    total_transactions: int
+    total_volume: Decimal = Field(..., decimal_places=2, max_digits=18)
+    avg_transaction_amount: Decimal = Field(..., decimal_places=2, max_digits=18)
     by_status: list[StatusSummary]
-    
+
+
 class TopUserResponse(BaseModel):
     user_id: UUID
-    total_spent: Decimal 
+    total_spent: Decimal = Field(..., decimal_places=2, max_digits=18)
     transaction_count: int
